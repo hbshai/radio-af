e4f9c1c155995fbab4a9a8cb2774ba188c85902b
Axel Smeets <murlocbrand+git@gmail.com>
2015-04-16 17:21:51 +0200

    setup instructions

---
+++ setup.bat
@@ -0,0 +1,14 @@
+cd ..
+
+start "phonegap setup" /W phonegap create -n "Radio AF" -i "se.designbyshai.radioaf" radio-af-app
+
+cd radio-af-app
+
+rmdir www /s /q
+
+mklink /j "www" ..\radio-af\www
+
+start "plugin-1" /W cordova plugin add org.apache.cordova.file
+start "plugin-2" /W cordova plugin add org.apache.cordova.media
+
+phonegap serve
